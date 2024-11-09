8c72af68661be02db2ca57af583d6f3b9b9fd8a7
Manoj9121github <128373138+Manoj9121github@users.noreply.github.com>
2024-11-09 16:07:13 +0530

    Add files via upload

---
+++ colorchanger.js
@@ -0,0 +1,16 @@
+let Elment=document.getElementById('body');
+
+function Clickblu(){    
+    Elment.style.backgroundColor='aqua';
+}
+
+function Clickgren(){
+    Elment.style.backgroundColor='green';
+}
+
+function Clickyelow(){
+    Elment.style.backgroundColor='yellow'
+}
+function Clickred(){
+    Elment.style.backgroundColor='red'
+}
